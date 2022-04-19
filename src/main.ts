1d419c3233b0e3d81565a7c4e8a0b0f60177a322
Amogh Lele <amogh@dyte.io>
2022-04-19 18:03:35 +0530

    feat(layout): set up basic structure
    Signed-off-by: Amogh Lele <amogh@dyte.io>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
@@ -6,3 +5,4 @@ app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+export{}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'modernist': ['Sk-Modernist', 'sans-serif'],
+        'sofia': ['Sofia Pro', 'sans-serif']
+      },
+      colors: {
+        'slate-blue': '#8873ef',
+      }
+    },
   },
   plugins: [],
 }
